41514e1a99717ea8a7874918fdfb7b98b4fbee14
Levinta Roman <roman.levinta@gmail.com>
2021-11-24 21:39:21 +0100

    no message

---
+++ ssg.py
@@ -0,0 +1,12 @@
+from typer import Typer
+import typer
+from ssg.site import Site
+
+
+def main(source='content', dest='dist'):
+    config = {"source": source, "dest": dest}
+    my_site = Site(**config)
+    my_site.build()
+
+
+typer.run(main)
