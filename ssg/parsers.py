feaa6eb6b184811ea63b6acb8c508476c4364faa
Levinta Roman <roman.levinta@gmail.com>
2022-01-18 20:34:47 +0100

    added Parser class

---
+++ ssg/parsers.py
@@ -0,0 +1,26 @@
+from typing import List
+from pathlib import Path
+import shutil
+
+
+class Parser:
+    def __init__(self):
+        self.extensions = List[str]
+
+    def valid_extension(self, extension):
+        return extension in self.extensions
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
+
+    def read(self, path):
+        with open(path, mode='r') as file:
+            return file.read()
+
+    def write(self, path, dest, content, ext='.html'):
+        full_path = f'{self.dest}/{path.with_suffix(ext).name}'
+        with open(full_path, mode='w') as file:
+            file.write(content)
+
+    def copy(self, path, source, dest):
+        shutil.copy2(path, dest)
