1480fa424a71576c74c3d2ea2538cd0f61d94065
Levinta Roman <roman.levinta@gmail.com>
2022-01-18 21:17:22 +0100

    refactor write method added ResourceParser

--- ssg.py
+++ ssg.py
@@ -1,10 +1,16 @@
 from typer import Typer
 import typer
 from ssg.site import Site
+import ssg.parsers
 
 
 def main(source='content', dest='dist'):
-    config = {"source": source, "dest": dest}
+    config = {
+        "source": source,
+        "dest": dest,
+        "parsers": [ssg.parsers.ResourceParser(), ],
+
+    }
     Site(**config).build()
 
 
