e3d2e92edfba571006ccacf073e1c1ecdb84fbec
Levinta Roman <roman.levinta@gmail.com>
2022-01-20 19:29:35 +0100

    added content class

---
+++ ssg/content.py
@@ -0,0 +1,14 @@
+import re
+from yaml import load, FullLoader
+from collections.abc import Mapping
+
+
+class Content(Mapping):
+    __delimeter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimeter, re.MULTILINE)
+
+    @classmethod
+    def load(cls, string):
+        [_, fm, content] = Content.__regex.split(string, 2)
+        metadata = load(fm, string)
+        return cls(metadata, content)
