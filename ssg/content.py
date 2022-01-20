897304d8a403c03e274e52f2915f72e71986fe6a
Levinta Roman <roman.levinta@gmail.com>
2022-01-20 19:45:27 +0100

    added content class

--- ssg/content.py
+++ ssg/content.py
@@ -16,3 +16,27 @@ class Content(Mapping):
     def __init__(self, metadata, content):
         self.data = metadata
         self.data["content"] = content
+
+    @property
+    def body(self):
+        return self.data["content"]
+
+    @property
+    def type(self):
+        return self.data["type"]
+
+    def __getitem__(self, item):
+        return self.data[item]
+
+    def __iter__(self):
+        self.data.iterate()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        data = {}
+        for key, value in self.data.items():
+            if key is not "content":
+                data[key] = value
+        return str(data)
